dd757b579920e4aee5be448c1341c7065742e8c2
stealthepaper <dasha.lotkova24@gmail.com>
2025-03-30 17:51:24 +0300

    second

--- indi_python/hello.py
+++ indi_python/hello.py
@@ -20,22 +20,42 @@
 #         print(f'Длинна слова {i} > 4')
 #         more_four.append(i)
 
+# products = 'apple, eggs, milk, orange, bread, caviar, banana, bear, grape, cream, chicken, wine, cheese, vinegar'
+# products = products.split(', ')
+# file_names = []
+# for i in products:
+#     if len(i) < 5:
+#         i += '.csv'
+#         file_names.append(i)
+#     else:
+#         i += '.xlsx'
+#         file_names.append(i)
+# xlsx_file = []
+# csv_file = []
+# for file in file_names:
+#     if 'xlsx' in file_names:
+#         xlsx_file.append(file)
+#     else:
+#         csv_file.append(file)
+#     print(f'Файл {file} успешно прочитан')
+
+# my_list = []
+# for i in range(1, 22, 2):
+#     i = i**2
+#     my_list.append(i)
+# print(my_list)
+
+# products = 'apple, eggs, milk, orange, bread, caviar, banana, bear, grape, cream, chicken, wine, cheese, vinegar'
+# products = products.split(', ')
+# for i in range(len(products)):
+#     print(i, products[i])
+
+
 products = 'apple, eggs, milk, orange, bread, caviar, banana, bear, grape, cream, chicken, wine, cheese, vinegar'
 products = products.split(', ')
-file_names = []
-for i in products:
-    if len(i) < 5:
-        i += '.csv'
-        file_names.append(i)
-    else:
-        i += '.xlsx'
-        file_names.append(i)
-print(file_names)
-xlsx_file = []
-csv_file = []
-for file in file_names:
-    if 'xlsx' in file_names:
-        xlsx_file.append(file)
-    else:
-        csv_file.append(file)
-    print(f'Файл {file} успешно прочитан')
+for ind, obj in enumerate(products):
+    print(ind, obj)
+
+products_dir = {"apple": 3, "eggs": 2, "milk": 4, "bread": 6, "banana": 8, "grape": 13, "cream": 5}
+for i in products_dir.items():
+    print(i)
