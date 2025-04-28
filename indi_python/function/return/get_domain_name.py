39cd761fbe5ab90dfba28e25cf0d75d1fc31b903
Дарья Лоткова <lotkova.d@office.lan>
2025-04-28 15:34:27 +0300

    domain

--- indi_python/function/return/get_domain_name.py
+++ indi_python/function/return/get_domain_name.py
@@ -1,17 +1,23 @@
-# str.rstrip() and  str.lstrip() 
+# Ваша задача написать функцию get_domain_name, которая принимает строку url, извлекает из нее доменное имя и возвращает его в качестве строки
 def get_domain_name (full_domain: str) -> str:
-    full_domain = full_domain.replace('https://www.', '')
+    full_domain = full_domain.replace('https://', '')
     full_domain = full_domain.replace('http://', '')
     full_domain = full_domain.split('.')
-    return full_domain[0]
-
-
+    for i in full_domain:
+        if i == 'www':
+            return full_domain[1]
+        else: 
+            return full_domain[0]
 
 
 print(get_domain_name("http://google.com")) #"google"
 print(get_domain_name("http://google.co.jp")) #"google"
+print(get_domain_name("www.xakep.ru")) #"xakep"
 print(get_domain_name("http://github.com/carbonfive/raygun"))
+print(get_domain_name("https://youtube.com")) #"youtube"
 print(get_domain_name("https://www.asos.com")) #"asos"
+print(get_domain_name("http://www.lenovo.com")) #"lenovo"
 print(get_domain_name("https://www.mywww.com")) # mywww
 print(get_domain_name("https://www.whatsapp.com")) # whatsapp
 print(get_domain_name("https://www.siemens.com"))
+print(get_domain_name("http://www.zombie-bites.com"))
